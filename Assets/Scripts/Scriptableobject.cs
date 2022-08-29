c09870d1dff97102af29e4ae0a1645b2b8af186e
joacoorqueda <101809787+joacoorqueda@users.noreply.github.com>
2022-08-29 12:08:23 -0300

    a mirar los videos de peruga que me perdi en la explicacion

---
+++ Assets/Scripts/Scriptableobject.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName ="NPCDataSO" ,menuName ="NPC Data")]
+public class Scriptableobject : MonoBehaviour
+{
+  public string[]
+
+}
